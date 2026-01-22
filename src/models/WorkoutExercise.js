5b3cfe1b422e7bd7c37c0cc42b92115a9ea23003
skl1menko <alklimenko227@gmail.com>
2026-01-22 13:51:29 +0200

    add WorkoutExerciseController. Add new method in WorkoutExercise model (removeExercieFromWorkout)

--- src/models/WorkoutExercise.js
+++ src/models/WorkoutExercise.js
@@ -41,6 +41,15 @@ class WorkoutExercise{
         return {deleted: result.rowCount};
     }
 
+    static async removeExerciseFromWorkout(workoutId, exerciseId){
+        const result = await pool.query(
+            `DELETE FROM workout_exercises
+            WHERE workout_id = $1 AND exercise_id = $2`,
+            [workoutId, exerciseId]
+        );
+        return {deleted: result.rowCount};
+    }
+
     static async reorderExercise(workoutId, exerciseOrder){
         const client = await pool.connect();
         try{
