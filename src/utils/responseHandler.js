4fd83a50b550dea21eb5fcc3e66c996afc64d48d
skl1menko <alklimenko227@gmail.com>
2026-01-26 15:22:34 +0200

    remove unusless methods

--- src/utils/responseHandler.js
+++ src/utils/responseHandler.js
@@ -10,16 +10,8 @@ const createResponse = (res, data, message = 'Created successfully') => {
     return successResponse(res, data, message, 201);
 };
 
-const noDataResponse = (res, message = 'No content') => {
-    return res.status(204).json({
-        status: 'success',
-        message,
-        data: []
-    });
-};
 
 module.exports = {
     successResponse,
-    createResponse,
-    noDataResponse
+    createResponse
 };
